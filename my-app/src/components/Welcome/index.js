316f5e70fc955d418d3853529bcc8f50a64fd4aa
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-20 17:12:17 +0530

    Added default Props for the Welcome Component

--- my-app/src/components/Welcome/index.js
+++ my-app/src/components/Welcome/index.js
@@ -11,12 +11,15 @@ class Welcome extends Component {
     this.setState((prevState) => ({ isLogin: !prevState.isLogin }));
   };
   render() {
+    const { name, greeting } = this.props;
     const { isLogin } = this.state;
     console.log(isLogin);
     return (
       <div className="main-container">
         <div className="card-container">
-          <h1>Hello User!</h1>
+          <h1>
+            {greeting} {name}!
+          </h1>
           <div>
             {isLogin ? (
               <button onClick={this.onChangeStatus}>LogOut</button>
@@ -29,5 +32,10 @@ class Welcome extends Component {
     );
   }
 }
+
+Welcome.defaultProps = {
+  name: "Badri Kumar",
+  greeting: "Hi",
+};
 
 export default Welcome;
